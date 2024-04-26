a53511a5275c34315f9de777c8c3f750a658f60b
008kevin <77583631+008kevin@users.noreply.github.com>
2024-04-26 11:23:06 +0200

    4. feladat

--- Fuvar.py
+++ Fuvar.py
@@ -1,19 +1,27 @@
 class Fuvar:
     def __init__(self, id, startTime, idotartam, distance, price, tip, paymentType) -> None:
-        self.id = id
+        self.id = int(id)
         self.startTime = startTime
         self.idotartam = idotartam
         self.distance = distance
-        self.price = price
-        self.tip = tip
+        self.price = float(price.replace(',', '.'))
+        self.tip = float(tip.replace(',', '.'))
         self.paymentType = paymentType
 
 fuvarList = []
 f = open('fuvar.csv', 'rt', encoding='utf-8')
 f.readline()
 for line in f:
-    line = line.strip().strip(';')
+    line = line.strip().split(';')
     fuvarList.append(Fuvar(line[0],line[1],line[2],line[3],line[4],line[5],line[6]))
 f.close()
 
 print(f"3. feladat: {len(fuvarList)} fuvar")
+
+fuvarCount = 0
+incomeCounter = 0
+for fuvar in fuvarList:
+    if fuvar.id == 6185:
+        fuvarCount += 1
+        incomeCounter += fuvar.price + fuvar.tip
+print(f"4. feladat: {fuvarCount} fuvar alatt: {str(incomeCounter).replace('.',',')}$")
